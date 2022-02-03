73776d1644176131ee293c69849e18994ec201d4
yi113 <yiyang2222@gmail.com>
2022-02-02 19:14:01 -0800

    Fail

---
+++ SkillDemo.java
@@ -0,0 +1,5 @@
+public class SkillDemo{
+    public static int intDivision(int x, int y){
+        return x / y;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import java.lang.annotation.Target;
+
+import org.junit.Test;
+
+public class SkillDemoTest {
+    @Test
+    public void division(){
+        assertEquals(3, SkillDemo.intDivision(5, 2));
+    }
+}
